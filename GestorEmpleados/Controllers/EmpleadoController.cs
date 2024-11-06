7b77da6ca6bb5bb291a4c50ec4980a18f7afe809
Jonathan Emmanuel <canedojonathan8@gmail.com>
2024-11-05 18:54:34 -0600

    Integracion Proyecto

---
+++ GestorEmpleados/Controllers/EmpleadoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiWebAPI.Data;
+using MiWebAPI.Models;
+
+namespace MiWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly EmpleadoData _empleadoData;
+        public EmpleadoController(EmpleadoData empleadoData)
+        {
+            _empleadoData = empleadoData;
+        }
+
+        [HttpPost]
+        [Route("GetEmpleados")]
+        public async Task<IActionResult> Lista([FromBody] string filtro)
+        {
+            List<Empleado> Lista = await _empleadoData.GetEmpleados(filtro);
+            return StatusCode(StatusCodes.Status200OK, Lista);
+        }
+
+        [HttpPost]
+        [Route("AddEmpleado")]
+        public async Task<IActionResult> AddEmpleado([FromBody] Empleado objeto)
+        {
+
+            var respuesta = await _empleadoData.AddEmpleado(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpPost]
+        [Route("UpdateEmpleado")]
+        public async Task<IActionResult> UpdateEmpleado([FromBody] Empleado objeto)
+        {
+
+            var respuesta = await _empleadoData.UpdateEmpleado(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpPost]
+        [Route("DeleteEmpleado")]
+        public async Task<IActionResult> DeleteEmpleado([FromBody] int Id)
+        {
+
+            var respuesta = await _empleadoData.DeleteEmpleado(Id);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+
+    }
+}
